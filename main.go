958c682dca6ba5ef50586f1d119052188cd843e5
wistia-richard <rdonbosco@wistia.com>
2022-10-21 03:11:42 -0400

    Fix checkout branch

--- main.go
+++ main.go
@@ -80,7 +80,9 @@ func main() {
 
 	password := os.Getenv("git_password")
 	fmt.Println(password)
-	if err := r.Push(&git.PushOptions{RemoteName: "origin", Auth: &http.BasicAuth{Username: "wistia-richard", Password: password}}); err != nil {
+	token := os.Getenv("token")
+	fmt.Println(token)
+	if err := r.Push(&git.PushOptions{RemoteName: "origin", Auth: &http.BasicAuth{Username: "wistia-richard", Password: token}}); err != nil {
 		log.Fatalf("Error unable push the commit to origin %s", err)
 	}
 	// token := os.Getenv("token")
